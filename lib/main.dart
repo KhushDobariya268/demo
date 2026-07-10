436903d49802fd42a4d662446f528ab468db813b
Khush Dobariya <khushdobariya2682007@gmail.com>
2026-07-10 20:01:05 +0530

    Current Date And Time

--- lib/main.dart
+++ lib/main.dart
@@ -59,13 +59,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
   //  var arrNames = ['Khush','Ravi,','Krishiv','Ashok','Vipul','Deep','Harshal','Khush','Ravi,','Krishiv','Ashok',];
 
+    var time = DateTime.now();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
         title: Text(widget.title),
       ),
-      body: Text("hello,khush")
+      body: Text('khush')
+      
+          // Current Date And Time
+     /*     
+      Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Current Time: ${time.hour}:${time.minute}:${time.second} ',style: TextStyle(fontSize: 25),
+              ),
+            ElevatedButton(onPressed: (){
+             setState(() {
+
+             });
+            },
+              child: Text('Current Time'),
+            ),
+            ],
+          ),
+        ),
+      )
+      
+      */
 
           // To Get User Input
 /*
